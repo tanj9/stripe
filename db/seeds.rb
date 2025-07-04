5fa0436720b2853ffc124074ccda4be35ea0b07b
Jerome Tan <jerome.tan@tuta.io>
2025-07-04 10:38:27 +0200

    Implemented authentication (with User and Session models)

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+puts 'Cleaning database...'
+Teddy.destroy_all
+Category.destroy_all
+
+puts 'Creating categories...'
+geek = Category.create!(name: 'geek')
+kids = Category.create!(name: 'kids')
+
+puts 'Creating teddies...'
+Teddy.create!(sku: 'original-teddy-bear', name: 'Teddy bear', category: kids, photo_url: 'https://miro.medium.com/v2/resize:fit:500/1*WXXnl4mWy6PxpkSkEqrGkQ.jpeg')
+Teddy.create!(sku: 'jean-mimi', name: 'Jean-Michel - Le Wagon', category: geek, photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
+Teddy.create!(sku: 'octocat',   name: 'Octocat -  GitHub',      category: geek, photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
+puts 'Finished!'
